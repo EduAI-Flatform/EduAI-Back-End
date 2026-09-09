6b6cbd1faf75bac198145baab12d57679bde7a2f
TDrake11 <141726163+TDrake11@users.noreply.github.com>
2026-09-09 12:19:34 +0700

    feat(commerce): register learner order history service

--- src/modules/commerce/commerce.module.ts
+++ src/modules/commerce/commerce.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from '../auth/auth.module';
 import { CommerceController } from './commerce.controller';
 import { CommerceService } from './commerce.service';
 import { CommerceOrderService } from './commerce-order.service';
+import { CommerceOrderHistoryService } from './commerce-order-history.service';
 import { VouchersModule } from '../vouchers/vouchers.module';
 import { CoursesModule } from '../courses/courses.module';
 import { CommerceAdminController } from './commerce-admin.controller';
@@ -14,7 +15,18 @@ import { CommerceProductService } from './commerce-product.service';
 @Module({
   imports: [PrismaModule, AuthModule, AuditModule, VouchersModule, CoursesModule],
   controllers: [CommerceController, CommerceAdminController],
-  providers: [CommerceService, CommerceOrderService, CommerceAdminService, CommerceProductService],
-  exports: [CommerceService, CommerceOrderService, CommerceProductService],
+  providers: [
+    CommerceService,
+    CommerceOrderService,
+    CommerceOrderHistoryService,
+    CommerceAdminService,
+    CommerceProductService,
+  ],
+  exports: [
+    CommerceService,
+    CommerceOrderService,
+    CommerceOrderHistoryService,
+    CommerceProductService,
+  ],
 })
 export class CommerceModule {}
